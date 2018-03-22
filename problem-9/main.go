0469f20d5b63b3221793fd499b536d4c0e39e6b9
Spiegel <spiegel.im.spiegel@gmail.com>
2018-03-22 13:23:07 +0900

    Fix typo

--- problem-9/main.go
+++ problem-9/main.go
@@ -11,14 +11,14 @@ import (
  * https://projecteuler.net/problem=9
  * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
  *   a^2 + b^2 = c^2
- * For example, 3^2 + 4^2 = 9 + 16 = 2^5 = 52.
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * Find the product abc.
  *
  * http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%209
  * ピタゴラス数(ピタゴラスの定理を満たす自然数)とは a < b < c で以下の式を満たす数の組である.
  *   a^2 + b^2 = c^2
- * 例えば, 3^2 + 4^2 = 9 + 16 = 2^5 = 52 である.
+ * 例えば, 3^2 + 4^2 = 9 + 16 = 25 = 5^2 である.
  * a + b + c = 1000 となるピタゴラスの三つ組が一つだけ存在する.
  * これらの積 abc を計算しなさい.
  *
