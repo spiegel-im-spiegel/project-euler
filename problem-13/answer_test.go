097498b9dc9d974b5615ceb988c652ee5a3f3594
Spiegel <spiegel.im.spiegel@gmail.com>
2018-04-11 13:16:49 +0900

    Add problem-13

---
+++ problem-13/answer_test.go
@@ -0,0 +1,39 @@
+package problem13
+
+import "testing"
+
+func TestAnswer0(t *testing.T) {
+	testCases := []struct {
+		answer string
+	}{
+		{answer: "5537376230"},
+	}
+
+	for _, tc := range testCases {
+		a := Answer0()
+		if a != tc.answer {
+			t.Errorf("Answer0() = %v, want %v.", a, tc.answer)
+		}
+	}
+}
+
+func BenchmarkAnswer0(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Answer0()
+	}
+}
+
+/* Copyright 2018 Spiegel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
